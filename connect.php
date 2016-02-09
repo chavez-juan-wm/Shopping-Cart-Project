ca8838a58715d73b0985dec1bc01998b641fa898
Juan Chavez <juan.chavez@west-mec.org>
2016-02-08 18:09:31 -0700

    Worked on the sign in and sign up page!

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@
     /*** mysql username ***/
     $username = 'root';
     /*** mysql password ***/
-    $password = '';
+    $password = 'root';
 
     try
     {
-        $dbh = new PDO("mysql:host=$hostname;dbname=shopping_cart", $username, $password);
+        $dbh = new PDO("mysql:host=$hostname;dbname=Shopping_Cart", $username, $password);
         // set the PDO error mode to exception
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
@@ -18,8 +18,8 @@
         echo "Connection failed: " . $e->getMessage();
     }
 
-    $query = "SELECT products.productName, users.firstName, users.lastName, users.email
-    FROM orders LEFT JOIN products on orders.productId = products.productId LEFT JOIN users on orders.userId = users.userId;";
+    $query = "SELECT Products.productName, Users.firstName, Users.lastName, Users.email, Orders.quantity
+    FROM Orders LEFT JOIN Products on Orders.productId = Products.productId LEFT JOIN Users on Orders.usersId = Users.usersId;";
 
 
     $stmt = $dbh->prepare($query);
@@ -35,10 +35,11 @@
     <body>
         <table align="center" border="1" style="background-color: white; border-radius: 10px; margin-left: auto; margin-right: auto; border-collapse: collapse;">
             <thead>
-                <th>Product Name</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Email</th>
+                <th>Product Name</th>
+                <th>Quantity</th>
             </thead>
 
             <tbody>
@@ -46,10 +47,11 @@
                 foreach($users as $user){
                     ?>
                     <tr>
-                        <td><?php echo $user['productName']?></td>
                         <td><?php echo $user['firstName']?></td>
                         <td><?php echo $user['lastName']?></td>
                         <td><?php echo $user['email']?></td>
+                        <td><?php echo $user['productName']?></td>
+                        <td><?php echo $user['quantity']?></td>
                     </tr>
                     <?php
                 }
